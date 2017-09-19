71f32d07c85c23c9f1ff4ae0f134b68333b6b54d
Norbert Czirjak <czirjak.norbert@gmail.com>
2017-09-19 23:08:05 +0200

    first commmit

---
+++ Smack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Norbert Czirjak on 2017. 09. 19..
+//  Copyright © 2017. Norbert Czirjak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
